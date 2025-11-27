51f994731398f6379b362427447e034bedba470c
Bibhuti Bhusan Padhi <bibhuti.mymail@gmail.com>
2025-11-27 15:39:28 +0530

    Added README and changes to main.py

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ from utilites import *
 
 user_input = "Reduce physical oracle refresh timeline by leveraging delphix"
 
-system_prompt1 = "WHW stands for What How Why framework of asking questions. Rewrite the below sentence in WHW framework\n" + user_input
-system_prompt2 = "How is it achieved currently ? Produce from knowledge and user to validate."
-system_prompt3 = "How is it achieved currently ? Generate related questionnaire for user to provide more context"
-system_prompt4 = "Gather knowledge on the technique"
-system_prompt5 = "Generate and suggest quantitative KPIs and validated by user"
-
-##print(system_prompt1)
-llmresponse1 = useLLM(system_prompt1)
-##llmresponse2 = useLLM(system_prompt2)
-##llmresponse3 = useLLM(system_prompt3)
-##llmresponse4 = useLLM(system_prompt4)
-##llmresponse5 = useLLM(system_prompt5)
-
+prompt1 = "WHW stands for What How Why framework of asking questions. Rewrite the below sentence in WHW framework \n Generate in below format \n **What:** \n **How:** \n **Why:**" + user_input
+llmresponse1 = useLLM(prompt1)
 print(llmresponse1)
+print("+ ---------------------------------------------- +")
+
+what = "Extract text from below statement for What \n The text starts after **What:** \n" + llmresponse1
+how = "Extract text from below statement of How \n The text starts after **How:** \n" + llmresponse1
+why = "Extract text from below statement of Why \n The text starts after **Why:** \n" + llmresponse1
+whatresponse = useLLM(what)
+howresponse = useLLM(how)
+whyresponse = useLLM(why)
+
+print(whatresponse)
+print(howresponse)
+print(whyresponse)
+
+print("+ ---------------------------------------------- +")
+
+prompt3 = whatresponse + "Extract the task from the above statement and answer the following. How is achieved currently ? Produce from knowledge and ask user to validate."
+llmresponse3 = useLLM(prompt3)
+print(llmresponse3)
+
+print("+ ---------------------------------------------- +")
+
+prompt4 = "How is " + whatresponse + " achieved currently ? Generate related questionnaire for user to provide more context"
+prompt5 = "Gather knowledge on the technique mentioned in : " + howresponse
+prompt6 = "Generate and suggest quantitative KPIs and validated by user"
+
